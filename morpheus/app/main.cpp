24694575d282fe1e05254c680479b2f275c537ec
mirmik <mirmikns@yandex.ru>
2023-02-05 22:41:57 +0300

    'scene'

--- morpheus/app/main.cpp
+++ morpheus/app/main.cpp
@@ -2,14 +2,15 @@
 #include <morpheus/SimpleView.h>
 #include <nos/trent/json_print.h>
 #include <rabbit/mesh/primitives.h>
+#include <igris/getopt/cliopts.h>
+
+#include <rabbit/mesh/primitives.h>
 
 #define GLEW_STATIC
 #include <GL/glew.h>
 
-// GLFW
 #include <GLFW/glfw3.h>
 
-// GLM Mathematics
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -17,10 +18,29 @@
 #include <chrono>
 #include <thread>
 
+void add_scene0( ModelServer & server )
+{
+    nos::println("add_scene0");
+    auto mesh = rabbit::box_mesh(1, 1, 1);
+    auto body = Body(mesh);
+    server.add_model(BodyId("box"), body);
+}
+
 int main(int argc, char **argv)
 {
+    igris::cliopts parser;
+    parser.add_string("scene", 's', "none");
+    parser.parse(argc, argv);
+
+    std::string scene = parser.get_string("scene");
+
     ModelServer server;
     server.start_server();
+
+    if (scene == "scene0")
+    {
+        add_scene0(server);
+    }
 
     SimpleView view(server);
     view.set_finalizer([&server]() { server.stop_server(); });
